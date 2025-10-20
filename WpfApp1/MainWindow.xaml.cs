76eeb6ff5e5d674dfe113ce0b4b336e231315e95
n1k1tal0x <d.cuzya07@gmail.com>
2025-10-20 15:28:09 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shell;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var chrome = WindowChrome.GetWindowChrome(this);
+            if (IsWindows11())
+                chrome.CornerRadius = new CornerRadius(28);
+            else
+                chrome.CornerRadius = new CornerRadius(0);
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private bool IsWindows11()
+        {
+            Version v = Environment.OSVersion.Version;
+            return v.Major == 10 && v.Build >= 22000;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e) {this.Close();}
+
+        private void Minimize_Click(object sender, RoutedEventArgs e) { this.WindowState = WindowState.Minimized; }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+Run Preview
+```
+cd BlazorPreview
+dotnet watch run
+```
+
+Run App
+```
+cd MyHybridApp
+dotnet build
+dotnet run --framework net9.0-windows10.0.19041.0
+```
+
+Run App Preview
+```
+cd MyHybridApp
+dotnet watch run --framework net9.0-windows10.0.19041.0
+```
